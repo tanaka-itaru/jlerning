d329e821f275fd8aa1509fed79dcb3dd09ad6330
tanaka-itaru <tanaka-itaru@dmm.com>
2017-05-24 20:09:22 +0900

    課題３：変数名を修正(validThru -> validThrough), getter/setterの追加

--- CreditCard.java
+++ CreditCard.java
@@ -8,18 +8,46 @@ public class CreditCard {
     private String  number;
     private String  memberName;
     private String  brand;
-    private Date    validThru;
+    private Date    validThrough;
 
-    public CreditCard(String icChip, String number, String memberName, String brand, Date validThru) {
+    public CreditCard(String icChip, String number, String memberName, String brand, Date validThrough) {
         this.icChip = icChip;
         this.number = number;
         this.memberName = memberName;
         this.brand = brand;
-        this.validThru = validThru;
+        this.validThrough = validThrough;
+    }
+
+    public String getIcChip(){
+        return icChip;
+    }
+
+    public String getNumber(){
+        return number;
+    }
+
+    public String getMemberName(){
+        return memberName;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public Date getValidThrough() {
+        return validThrough;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public void setValidThrough(Date validThrough) {
+        this.validThrough = validThrough;
     }
 
     public boolean isValid(Date targetDate){
-        return this.validThru.after(targetDate) | this.validThru.equals(targetDate);
+        return this.validThrough.after(targetDate) | this.validThrough.equals(targetDate);
     }
 
     //カード番号が同じならtrueとする場合
